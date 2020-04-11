b7f19c79149a08fdc04932a4e1c7a314527d284e
Gary Llil <garyjllil@gmail.com>
2020-04-11 16:22:37 -0400

    created server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+const routes = require("./controllers/burgers_controller")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
